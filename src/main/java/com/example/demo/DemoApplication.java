8f53ba93423e96eaeb1fa716a28ff3cd2ebba584
Gary Smith <garysmith193358@gmail.com>
2024-01-10 17:42:15 -0500

    added student controller and student services

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,8 +13,5 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
-	}
+
 }
